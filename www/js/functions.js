4a3d96644ece8af50c126d7984a1217bf8344f12
DusanNesic95 <dusannesic001@gmail.com>
2016-10-22 16:48:14 +0200

    Dodata geolokacija

--- www/js/functions.js
+++ www/js/functions.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+
+  var lat = 0;
+  var long = 0;
+  navigator.geolocation.getCurrentPosition(function(position) {
+      lat = position.coords.latitude;
+      long = position.coords.longitude;
+  }, function() {
+      alert("Your phone does not support GPS location!");
+  }, {maximumAge:60000, timeout:15000, enableHighAccuracy:true});
+
   var type = localStorage.getItem('type');
   var name = "";
   var number = "";
@@ -11,7 +21,8 @@ $(document).ready(function() {
     localStorage.setItem('type', 'receive');
   });
 
-  $("#submitBtn").click(function() {
+  $("#submit").click(function() {
+    console.log(lat, long);
     name = $("#name").val();
     number = $("#number").val();
     if (name == "" || number == "") {
@@ -28,14 +39,34 @@ $(document).ready(function() {
   });
 });
 
-function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-}
+// function initMap() {
+//   var uluru = {lat: -25.363, lng: 131.044};
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 4,
+//     center: uluru
+//   });
+//   var marker = new google.maps.Marker({
+//     position: uluru,
+//     map: map
+//   });
+// }
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  console.log('Function is called againp!');
+  var crd = pos.coords;
+
+  alert('Your current position is:');
+  alert('Latitude : ' + crd.latitude);
+  alert('Longitude: ' + crd.longitude);
+  alert('More or less ' + crd.accuracy + ' meters.');
+};
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
